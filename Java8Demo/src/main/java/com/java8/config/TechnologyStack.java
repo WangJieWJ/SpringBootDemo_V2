120b9f5789cd01df1c41c98105b35e347a509673
wangjie <1152689483@qq.com>
2019-05-05 17:53:51 +0800

    并发工具类

---
+++ Java8Demo/src/main/java/com/java8/config/TechnologyStack.java
@@ -0,0 +1,29 @@
+package com.java8.config;
+
+/**
+ * Title:
+ * Description:
+ * Copyright: 2019 北京拓尔思信息技术股份有限公司 版权所有.保留所有权
+ * Company:北京拓尔思信息技术股份有限公司(TRS)
+ * Project: SpringBootDemo
+ * Author: 王杰
+ * Create Time:2019/5/5 17:39
+ */
+public enum TechnologyStack {
+
+    /**
+     * 初始化必须放在最前面
+     */
+    SPRING("2013", "Spring"),
+    SPRING_BOOT("2014", "SpringBoot"),
+    SPRING_CLOUD("2015", "SpringCloud"),
+    DOUBLE("2016", "Double");
+
+    private final String year;
+    private final String name;
+
+    TechnologyStack(String year, String name) {
+        this.year = year;
+        this.name = name;
+    }
+}
